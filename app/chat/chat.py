864faeac7b56986149c1cc1bf69e26e5c907a045
Sudhanva Narayana <nsudhanva@gmail.com>
2024-01-14 13:22:22 -0500

    add retreiver chain

--- app/chat/chat.py
+++ app/chat/chat.py
@@ -1,4 +1,5 @@
 from app.chat.models import ChatArgs
+from app.chat.vectorstores.pinecone import build_retriever
 
 
 def build_chat(chat_args: ChatArgs):
@@ -13,4 +14,4 @@ def build_chat(chat_args: ChatArgs):
         chain = build_chat(chat_args)
     """
 
-    pass
+    retriever = build_retriever(chat_args)
