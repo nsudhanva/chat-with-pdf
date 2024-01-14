53b57da1baf4445657ac9691498c21a29ce83174
Sudhanva Narayana <nsudhanva@gmail.com>
2024-01-14 15:05:55 -0500

    Chatting works with context

--- app/chat/chat.py
+++ app/chat/chat.py
@@ -1,17 +1,13 @@
 from app.chat.models import ChatArgs
-from app.chat.vectorstores.pinecone import build_retriever
+from app.chat.vector_stores.pinecone import build_retriever
+from langchain.chains import ConversationalRetrievalChain
+from app.chat.llm.chatopenai import build_llm
+from app.chat.memories.sql_memory import build_memory
 
 
 def build_chat(chat_args: ChatArgs):
-    """
-    :param chat_args: ChatArgs object containing
-        conversation_id, pdf_id, metadata, and streaming flag.
-
-    :return: A chain
-
-    Example Usage:
-
-        chain = build_chat(chat_args)
-    """
-
     retriever = build_retriever(chat_args)
+    llm = build_llm(chat_args)
+    memory = build_memory(chat_args)
+
+    return ConversationalRetrievalChain.from_llm(llm=llm, retriever=retriever, memory=memory)
